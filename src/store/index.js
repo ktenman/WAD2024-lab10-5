de9fdbf7dedbd82b559f42ab7cde2ffdc5464c33
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-11-13 16:43:45 +0200

    Move logic from Products.vue to get data from Vue store

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,14 @@ import { createStore } from 'vuex'
 export default createStore({
     strict: true,
 
-    state: {},
+    state: {
+        productList: [
+            {id: 1, author: "Charlotte Bronte", book: "Jane Eyre", goodreads: "4.1/5", price: 20},
+            {id: 2, author: "Margaret Mitchell", book: "Gone with the Wind", goodreads: "4.3/5", price: 22},
+            {id: 3, author: "Anthony Burgess", book: "A Clockwork Orange", goodreads: "4/5", price: 13},
+            {id: 4, author: "Fyodor Dostoevsky", book: "Crime and Punishment", goodreads: "4.2/5", price: 18}
+        ]
+    },
     getters: {},
     mutations: {},
     actions: {}
